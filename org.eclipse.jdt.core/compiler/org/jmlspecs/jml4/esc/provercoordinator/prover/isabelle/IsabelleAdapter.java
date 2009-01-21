019d93221f4bf6c5ac6c12ef7af1e4c9b6b2324c
CJ Sheu <c_sheu@encs.concordia.ca>
2009-01-21 15:32:43 +0000

    undo commit from version 269 to 225

--- org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/esc/provercoordinator/prover/isabelle/IsabelleAdapter.java
+++ org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/esc/provercoordinator/prover/isabelle/IsabelleAdapter.java
@@ -77,7 +77,6 @@ public class IsabelleAdapter extends ProverAdapter {
 	}
 
 	private Result[] prove(String theoryFilePathWithoutExt, boolean isOuaEsc) {
-		//Process process = getProverProcess();
 		synchronized(lock) {
 			getProverProcess();
 			if (process == null) {
@@ -102,25 +101,30 @@ public class IsabelleAdapter extends ProverAdapter {
 				InputStream in = process.getInputStream();
 				BufferedReader bIn = new BufferedReader(new InputStreamReader(in));
 				String line = bIn.readLine();
-				while (! (line.contains(EOF_1) || line.endsWith(OOPS) || line.startsWith(EOF_2)) ) {
+				while (! (line.equals(EOF_1) || line.endsWith(OOPS) || line.startsWith(EOF_2)) ) {
 					buffer.append(line + "\n"); //$NON-NLS-1$
 					line = bIn.readLine();
 				}
 				buffer.append(line + "\n");
-
+				//read all data unread in the inputstream buffer
+				byte [] response = new byte[in.available()];
+				int i = in.read(response);
+				
+				if (DEBUG)
+					Logger.print(buffer.toString());
+				Result[] result = formatResult(buffer.toString());
+				if (!isOuaEsc && Result.isValid(result))
+					Utils.deleteFile(theoryFilePath);
+				
+				return result;
+				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
 				lock.notify();
 			}
-			if (DEBUG)
-				Logger.print(buffer.toString());
-			Result[] result = formatResult(buffer.toString());
-			if (!isOuaEsc && Result.isValid(result))
-				Utils.deleteFile(theoryFilePath);
-			
-			lock.notify();
-			return result;
+			return new Result[0];
 		}
 	}
 
@@ -168,10 +172,7 @@ public class IsabelleAdapter extends ProverAdapter {
 	public static /*@nullable*/Process getProverProcess() {
 		if(process == null) {
 			try {
-				long t = System.currentTimeMillis();
 				process =  Runtime.getRuntime().exec(isabelleCmd());
-				long t2 = System.currentTimeMillis();
-				System.out.println("" + (t2 - t));
 				return process;
 			} catch (IOException e) {
 				System.err.println(e);

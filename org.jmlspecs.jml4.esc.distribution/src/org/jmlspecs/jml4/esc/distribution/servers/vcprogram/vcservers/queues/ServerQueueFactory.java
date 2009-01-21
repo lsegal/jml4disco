c6ab7c03ae5fa341b09a60f6cb8e4b5e55d4d728
Loren Segal <lsegal@soen.ca>
2009-01-21 16:25:52 +0000

    Move distribution code into trunk

---
+++ org.jmlspecs.jml4.esc.distribution/src/org/jmlspecs/jml4/esc/distribution/servers/vcprogram/vcservers/queues/ServerQueueFactory.java
@@ -0,0 +1,57 @@
+package org.jmlspecs.jml4.esc.distribution.servers.vcprogram.vcservers.queues;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.jmlspecs.jml4.esc.distribution.servers.vcprogram.vcservers.implementations.RemoteTomCatServer;
+
+/**
+ * This factory class will return an instance of SeverQueue. If no instance yet
+ * has been created, it will create one and initialize it.
+ */
+public final class ServerQueueFactory {
+
+	private static final String PROPERTIES_FILE = "dispatch"; // To initialize
+																// the servers
+	private static ServerQueue serverRegistry = null;
+
+	private ServerQueueFactory() {
+
+	}
+
+	/**
+	 * @return Will return an ServerQueue instance
+	 */
+	public static final ServerQueue getRemoteProveVcServerQueueInstance() {
+
+		if (serverRegistry == null)
+			serverRegistry = initServers();
+		return serverRegistry;
+	}
+
+	/**
+	 * This initializes the servers with the data from the configuration file
+	 * 
+	 * @return A new instance of RemoteProveVcServerQueue with the server data
+	 *         initialized from the properties file.
+	 */
+	private static ServerQueue initServers() {
+		try {
+			ResourceBundle bundle = ResourceBundle.getBundle(PROPERTIES_FILE);
+
+			// TODO check that numberOfServers key exists
+			int numberOfServers = Integer.parseInt(bundle
+					.getString("numberOfServers"));
+			ServerQueue newRegistry = new ServerQueue(numberOfServers);
+
+			for (int i = 1; i <= numberOfServers; i++) {
+				newRegistry.add(new RemoteTomCatServer(bundle
+						.getString("vcServer." + i)));
+			}
+			return newRegistry;
+		} catch (MissingResourceException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}

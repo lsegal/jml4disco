f4a36eb84a5b1db856c827ac0b37b5a42e314777
Richard Le Guen <r_leguen@encs.concordia.ca>
2009-02-03 18:17:22 +0000

    Renamed project from 'DickieEsc4Web'

--- org.jmlspecs.jml4.esc.distribution.http/src/org/jmlspecs/jml4/esc/distribution/http/servlets/ProveVc.java
+++ org.jmlspecs.jml4.esc.distribution.http/src/org/jmlspecs/jml4/esc/distribution/http/servlets/ProveVc.java
@@ -1,9 +1,8 @@
-package org.jmlspecs.jml4.esc.distribution.www.servlets;
-
-
+package org.jmlspecs.jml4.esc.distribution.http.servlets;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.ObjectOutputStream; 
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -12,19 +11,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jmlspecs.jml4.esc.Esc;
-import org.jmlspecs.jml4.esc.distribution.servers.vcprogram.VcProgramDispatchingServer;
+//import org.jmlspecs.jml4.esc.distribution.ServerProfile;
+import org.jmlspecs.jml4.esc.distribution.ServerProfile;
+import org.jmlspecs.jml4.esc.distribution.servers.vc.ProveVcServer;
 import org.jmlspecs.jml4.esc.result.lang.Result;
-import org.jmlspecs.jml4.esc.vc.lang.VcProgram;
+import org.jmlspecs.jml4.esc.vc.lang.VC;
 
 /**
- * Servlet implementation class ProveVcProgram
+ * Servlet implementation class ProveVc
  */
-public class ProveVcProgram extends HttpServlet {
+public class ProveVc extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static final int NUMBER_OF_THREADS = 64;
-       
-	public void init(ServletConfig config) throws ServletException {
-	    super.init(config);
+	
+	public synchronized void init(ServletConfig config) throws ServletException {
+	    super.init(config);  
 	}   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
@@ -32,37 +32,42 @@ public class ProveVcProgram extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
 	}
-	
 	private static int id = (int)(Math.random() * 10000);
-   protected void processRequest (HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-	   
-	   System.out.println("DickieESC4Web processRequest");
+	
+   @SuppressWarnings("unchecked")
+    protected void processRequest (HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 	   
 	   Esc.GEN_STATS = false;
+	   
 		String contentType = "application/x-java-serialized-object";  
-		response.setContentType(contentType);
-		
+
 		Result[] result = Result.EMPTY;
 		ObjectInputStream in = null;
-		VcProgram vcProg;
+		VC vc = null;
+		Map<String, Integer> map;
 		try {
 			if (request.getContentLength() != -1) {
 				in = new ObjectInputStream(request.getInputStream());
-				vcProg = (VcProgram) in.readObject();
-				result = VcProgramDispatchingServer.proveVcProgram(vcProg);
-			}			
+				vc = (VC) in.readObject();
+				map = (Map<String, Integer>) in.readObject();
+				result = ProveVcServer.prove(vc, map);
+			} 
 
-		} catch (Exception e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 		
 		response.setContentType(contentType);
 		ObjectOutputStream out = new ObjectOutputStream(response.getOutputStream());  	   
 		out.writeObject(result);
+		ServerProfile profile = new ServerProfile(); // added by Dickie on Fri Nov 28th 2:00pm-ish
+		out.writeObject(profile);
 		out.flush();
 		out.close();
-		
-		System.out.println("DONE DickieESC4Web processRequest");
-
-	}
+			   
+//		   response.setContentType("text/html");
+//		   PrintWriter out = response.getWriter();
+//		   out.println("Hello web (from the servlet)! "+ value);
+//		   out.close();	   
+   		}
 }

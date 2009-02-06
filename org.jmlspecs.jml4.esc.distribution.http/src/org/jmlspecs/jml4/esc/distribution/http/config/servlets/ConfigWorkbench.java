d9f44355ce8de2ba3a21461944e571565e2a30ca
Richard Le Guen <r_leguen@encs.concordia.ca>
2009-02-06 12:34:50 +0000

    more config stuff

--- org.jmlspecs.jml4.esc.distribution.http/src/org/jmlspecs/jml4/esc/distribution/http/config/servlets/ConfigWorkbench.java
+++ org.jmlspecs.jml4.esc.distribution.http/src/org/jmlspecs/jml4/esc/distribution/http/config/servlets/ConfigWorkbench.java
@@ -41,23 +41,46 @@ public class ConfigWorkbench extends HttpServlet {
 					in = in.substring(1);
 				}
 
-				if(new File(getServletConfig().getServletContext().getRealPath("/WEB-INF/config-workbench/"+in+".get.jsp")).exists()) {
+				String path = "/WEB-INF/config-workbench/"+in+".get.jsp";
+				String filename = getServletConfig().getServletContext().getRealPath(path);
+				File f = new File(filename); 
+				if(f.exists()) {
 					PrintWriter response_out = response.getWriter();
 					response_out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">");
 					response_out.println("<html>");
 					response_out.println("<head>");
 					response_out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">");
 					response_out.println("<title>JML4 Disco Configuration Interface | "+in+"</title>");
-					response_out.println("<title>JML4 Disco Configuration Interface | "+in+"</title>");
+					response_out.println("<meta name='keywords' content='' />");
+					response_out.println("<meta name='description' content='' />");
+					response_out.println("<link href='default.css' rel='stylesheet' type='text/css' />");
 					response_out.println("</head>");
 					response_out.println("<body>");
+					response_out.println("<div id='header'>");
+					response_out.println("<div id='menu'>");
+					response_out.println("	<ul>");
+					response_out.println("		<li><a href='index' title=''>JML4 Disco - Server Config</a></li>");
+					response_out.println("		<li><a href='AddServers' title=''>Add Servers</a></li>");
+					response_out.println("	</ul>");
+					response_out.println("</div>");
+					response_out.println("</div>");
+					response_out.println("<div>");
 					getServletConfig().getServletContext().getRequestDispatcher("/WEB-INF/config-workbench/"+in+".get.jsp").include(request,response);
+					response_out.println("</div>");
 					response.getWriter().println("<!--added from the servlet-->");
 					response_out.println("</body>");
 					response_out.println("</html>");
 				}
 				else {
-					response.setStatus(404);
+					path = "/WEB-INF/config-workbench/"+in;
+					filename = getServletConfig().getServletContext().getRealPath(path);
+					f = new File(filename); 
+					if(f.exists()) {
+						getServletConfig().getServletContext().getRequestDispatcher(path).include(request,response);
+					}
+					else {
+						response.setStatus(404);
+					}
 				}
 			}
 			else {

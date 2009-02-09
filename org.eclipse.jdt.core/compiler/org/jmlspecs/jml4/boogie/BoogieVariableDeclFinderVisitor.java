d743bff4ac675cde4a4df808fa270c8f66fd3b84
Leveda Giannas <leveda@encs.concordia.ca>
2009-02-09 16:20:22 +0000

    Ticket No#156 added functionallity to boogieVisitor to collect and print all localdeclarations at the beginning of a method future work: remove flag and implement differently (visit(localdeclaration))

---
+++ org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/BoogieVariableDeclFinderVisitor.java
@@ -0,0 +1,28 @@
+package org.jmlspecs.jml4.boogie;
+
+import java.util.ArrayList;
+
+import org.eclipse.jdt.internal.compiler.ASTVisitor;
+import org.eclipse.jdt.internal.compiler.ast.LocalDeclaration;
+import org.eclipse.jdt.internal.compiler.ast.MethodDeclaration;
+import org.eclipse.jdt.internal.compiler.lookup.BlockScope;
+import org.eclipse.jdt.internal.compiler.lookup.ClassScope;
+
+public class BoogieVariableDeclFinderVisitor extends ASTVisitor {
+
+	ArrayList locals = new ArrayList();
+	
+	public boolean visit(MethodDeclaration methodDeclaration, ClassScope scope) {
+		for(int i=0; i< methodDeclaration.statements.length; i ++) {
+			methodDeclaration.statements[i].traverse(this, methodDeclaration.scope);
+		}
+		return true;
+	}
+	public boolean visit(LocalDeclaration localDecl, BlockScope scope) {
+		locals.add(localDecl);  
+		return true;
+	}
+	public ArrayList getDecls() {
+		return locals;
+	}
+}

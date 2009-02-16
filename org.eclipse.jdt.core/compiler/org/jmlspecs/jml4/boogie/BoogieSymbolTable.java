05aa5a6b448f55079a36021cdcd064ae0afa1aa7
Loren Segal <lsegal@soen.ca>
2009-02-16 13:37:52 +0000

    Add BoogieSymbolTable with tests (see #158)

---
+++ org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/BoogieSymbolTable.java
@@ -0,0 +1,76 @@
+package org.jmlspecs.jml4.boogie;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class BoogieSymbolTable {
+	private final static String charmap = "abcdefghijklmnopqrstuvwxyz"; //$NON-NLS-1$
+	private String generatedSymbol = ""; //$NON-NLS-1$
+	private ArrayList scope = new ArrayList();
+	
+	public BoogieSymbolTable() {
+		enterScope();
+	}
+	
+	public void enterScope() {
+		scope.add(new Hashtable());
+	}
+	
+	public void exitScope() {
+		scope.remove(scope.size() - 1);
+	}
+	
+	public synchronized void addSymbol(String symbol) {
+		Hashtable lastScope = (Hashtable)scope.get(scope.size() - 1);
+		if (lastScope.get(symbol) != null) {
+			throw new IllegalArgumentException("Symbol " + symbol + " already exists"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		lastScope.put(symbol, generateSymbol());
+	}
+	
+	public String lookup(String symbol) {
+		// look through scopes (last to first)
+		for (int i = scope.size() - 1; i >= 0; i--) {
+			Hashtable tab = (Hashtable)scope.get(i);
+			String val = (String)tab.get(symbol);
+			if (val != null) return val;
+		}
+		return null;
+	}
+	
+	private synchronized String generateSymbol() {
+		if (generatedSymbol == "") { //$NON-NLS-1$
+			generatedSymbol = new String(new char[]{charmap.charAt(0)}, 0, 1);
+			return generatedSymbol;
+		}
+		
+		char[] sym = generatedSymbol.toCharArray();
+		for (int symindex = sym.length - 1; symindex >= 0; symindex--) {
+			char c = sym[symindex];
+			int index = charmap.indexOf(c);
+			if (index + 1 >= charmap.length()) {
+				c = charmap.charAt(0);
+				if (symindex == 0) {
+					// increment the string length ("zzz" turns into "aaaa")
+					sym = new char[sym.length + 1];
+					for (int i = 0; i < sym.length; i++) {
+						sym[i] = c;
+					}
+					break;
+				}
+				
+				// make everything else "a" ("azzz" goes to "baaa")
+				for (int i = symindex; i < sym.length; i++) { 
+					sym[i] = c;
+				}
+			}
+			else {
+				sym[symindex] = charmap.charAt(index + 1);
+				break;
+			}
+		}
+		
+		generatedSymbol = new String(sym);
+		return generatedSymbol;
+	}
+}

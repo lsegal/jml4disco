85402b46ea9d94413fd03901182d7fc71900b594
Loren Segal <lsegal@soen.ca>
2009-02-19 12:32:53 +0000

    Document BoogieSymbolTable

--- JML4/dev/trunk/org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/BoogieSymbolTable.java
+++ JML4/dev/trunk/org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/BoogieSymbolTable.java
@@ -3,7 +3,13 @@ package org.jmlspecs.jml4.boogie;
 import java.util.Hashtable;
 
 import org.eclipse.jdt.internal.compiler.ast.Block;
+import org.eclipse.jdt.internal.compiler.ast.MethodDeclaration;
 
+/**
+ * Maintains and passifies local variable symbols in a {@link MethodDeclaration}.
+ * Passification means that each local variable will have a block-independent, unique
+ * symbol representing the variable name.
+ */
 public class BoogieSymbolTable {
 	private final static String charmap = "abcdefghijklmnopqrstuvwxyz"; //$NON-NLS-1$
 	private String generatedSymbol = ""; //$NON-NLS-1$
@@ -15,10 +21,20 @@ public class BoogieSymbolTable {
 		enterScope(new Block(0));
 	}
 	
+	/**
+	 * The current block representing the variable scope.
+	 * 
+	 * @return the current block
+	 */
 	public Block getCurrentBlock() {
 		return currentBlock;
 	}
 	
+	/**
+	 * Enters a new block (which is used to represent variable scope), updating the current block.
+	 * 
+	 * @param block the block to enter
+	 */
 	public void enterScope(Block block) {
 		if (currentBlock != null) { 
 			heirarchy.put(block, currentBlock);
@@ -30,10 +46,20 @@ public class BoogieSymbolTable {
 		}
 	}
 	
+	/**
+	 * Exits the current scope (block) by setting current block to
+	 * its parent block.
+	 */
 	public void exitScope() {
 		currentBlock = (Block)heirarchy.get(currentBlock);
 	}
 	
+	/**
+	 * Adds a symbol to the current block
+	 * @param symbol the symbol to add
+	 * @return the generated passified representation of the symbol (same value
+	 * 		as subsequent calls to {@link #lookup(String)})
+	 */
 	public synchronized String addSymbol(String symbol) {
 		Hashtable lastScope = (Hashtable)scope.get(currentBlock);
 		if (lastScope.get(symbol) != null) {
@@ -44,10 +70,26 @@ public class BoogieSymbolTable {
 		return value;
 	}
 	
+	/**
+	 * Looks for a symbol name associated with the last entered block 
+	 * (via {@link #enterScope(Block)}), {@link #getCurrentBlock()}.  
+	 * @param symbol the symbol name to look for
+	 * @return the unique block-independent symbol representation for the
+	 * 		   symbol name.
+	 * @see #lookup(String, Block)
+	 */
 	public String lookup(String symbol) {
 		return lookup(symbol, currentBlock);
 	}
 
+	/**
+	 * Looks for a symbol name associated with a specific block scope and
+	 * returns the unique symbol value representing it.
+	 * 
+	 * @param symbol the symbol name
+	 * @param block the block object maintaining symbol scopes 
+	 * @return the passified unique symbol value
+	 */
 	public String lookup(String symbol, Block block) {
 		// look through scopes (last to first)
 		while (block != null) {
@@ -59,6 +101,11 @@ public class BoogieSymbolTable {
 		return null;
 	}
 	
+	/**
+	 * Generates a new unique symbol name, also storing it as {@link #generatedSymbol}.
+	 *  
+	 * @return the next symbol value (the value of {@link #generatedSymbol})
+	 */
 	private synchronized String generateSymbol() {
 		if (generatedSymbol == "") { //$NON-NLS-1$
 			generatedSymbol = new String(new char[]{charmap.charAt(0)}, 0, 1);

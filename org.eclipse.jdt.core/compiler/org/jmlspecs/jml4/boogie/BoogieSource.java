952f8a5aecfeeb6fad470a4b12c576635502a6c5
Loren Segal <lsegal@soen.ca>
2009-03-04 11:29:30 +0000

    Fix formatting and warnings

--- org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/BoogieSource.java
+++ org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/BoogieSource.java
@@ -5,8 +5,8 @@ import java.util.Hashtable;
 import org.eclipse.jdt.internal.compiler.ast.ASTNode;
 
 /**
- * Represents a buffer of Boogie source code that will be
- * passed to a Boogie runtime object. 
+ * Represents a buffer of Boogie source code that will be passed to a Boogie
+ * runtime object.
  * 
  * To return the actual source code use {@link #getResults()}.
  */
@@ -20,8 +20,8 @@ public class BoogieSource {
 	private int prependsOffset;
 	private static StringBuffer headers;
 	private StringBuffer prepends;
-	
-	public BoogieSource(){
+
+	public BoogieSource() {
 		prepends = new StringBuffer();
 		implBody.append(getHeaders());
 		sourcePoint.row += headersOffset;
@@ -39,8 +39,8 @@ public class BoogieSource {
 	}
 
 	/**
-	 * This method is to ensure that the boogie source points point to the corresponding
-	 * ASTNodes after any prepends have been added.
+	 * This method is to ensure that the boogie source points point to the
+	 * corresponding ASTNodes after any prepends have been added.
 	 */
 	private void adjustSourcePoints() {
 		Object[] keys = pointTable.keySet().toArray();
@@ -63,38 +63,43 @@ public class BoogieSource {
 	/**
 	 * Returns the node associated with a point in a Boogie source file.
 	 * 
-	 * @param start a position in a Boogie source file
-	 * @return the node which was mapped to a specific point in a Boogie source file
+	 * @param start
+	 *            a position in a Boogie source file
+	 * @return the node which was mapped to a specific point in a Boogie source
+	 *         file
 	 */
 	public ASTNode getTermAtPoint(BoogieSourcePoint start) {
-		return (ASTNode)pointTable.get(start);
+		return (ASTNode) pointTable.get(start);
 	}
-	
+
 	/**
-	 * Maps the current position in a Boogie source file to a specific Java ASTNode object.
+	 * Maps the current position in a Boogie source file to a specific Java
+	 * ASTNode object.
 	 * 
-	 * @param term the AST node object to map
+	 * @param term
+	 *            the AST node object to map
 	 */
 	public void addPoint(ASTNode term) {
 		pointTable.put(sourcePoint.clone(), term);
 	}
 
 	/**
-	 * Convenience method to append contents to the buffer without performing any 
-	 * Boogie source point mapping. Equivalent to <tt>append(o, null);</tt>.
+	 * Convenience method to append contents to the buffer without performing
+	 * any Boogie source point mapping. Equivalent to <tt>append(o, null);</tt>.
 	 * 
-	 * @param o the object to append
+	 * @param o
+	 *            the object to append
 	 */
 	public void append(Object o) {
 		append(o, null);
 	}
 
 	public void preprend(Object o) {
-		//Ensures that everything that is prepended is on it's own line
-		//And counts the number of lines that have been prepended.
-		if (((String) o).charAt(((String) o).length()-1) != '\n')
-			o = ((String) o) + "\n";
-		String[] num = ((String) o).split("\\n");
+		// Ensures that everything that is prepended is on it's own line
+		// And counts the number of lines that have been prepended.
+		if (((String) o).charAt(((String) o).length() - 1) != '\n')
+			o = ((String) o) + "\n"; //$NON-NLS-1$
+		String[] num = ((String) o).split("\\n"); //$NON-NLS-1$
 		prepends.insert(0, o);
 		prependsOffset += num.length;
 	}
@@ -104,15 +109,15 @@ public class BoogieSource {
 			populateHeaders();
 		return headers.toString();
 	}
-	
+
 	/**
-	 * These are the current headers necessary for division and modulus operations to work.
-	 * Should eventually be moved into it's own file.
+	 * These are the current headers necessary for division and modulus
+	 * operations to work. Should eventually be moved into it's own file.
 	 */
 	private static void populateHeaders() {
 		headers = new StringBuffer();
-		headers.append("axiom (∀ x : int, y: int • {x % y} {x /y} x%y == x - x/y *y);\n");
-		headers.append("axiom (∀x:int,y:int•{x%y}(0<y⇒0<=x%y∧x%y<y)∧(y<0⇒y<x%y∧x%y<=0));\n");
+		headers.append("axiom (∀ x : int, y: int • {x % y} {x /y} x%y == x - x/y *y);\n"); //$NON-NLS-1$
+		headers.append("axiom (∀x:int,y:int•{x%y}(0<y⇒0<=x%y∧x%y<y)∧(y<0⇒y<x%y∧x%y<=0));\n"); //$NON-NLS-1$
 		headersOffset = 2;
 	}
 
@@ -128,10 +133,15 @@ public class BoogieSource {
 	 * highlighting of the invalid expression through Eclipse's problem reporter
 	 * API.
 	 * 
-	 * @param o the object to output to the buffer ({@link #toString()} is called on the object)
-	 * @param linePointTerm used to map the current position in the Boogie source to a Java AST node
-	 * 		in order to map a position in a Boogie source file to a Java source file (during problem reporting).
-	 * 		If this object is null, no mapping will be done at the current position.
+	 * @param o
+	 *            the object to output to the buffer ({@link #toString()} is
+	 *            called on the object)
+	 * @param linePointTerm
+	 *            used to map the current position in the Boogie source to a
+	 *            Java AST node in order to map a position in a Boogie source
+	 *            file to a Java source file (during problem reporting). If this
+	 *            object is null, no mapping will be done at the current
+	 *            position.
 	 */
 	public void append(Object o, ASTNode linePointTerm) {
 		if (newLine && indent > 0) {
@@ -140,21 +150,24 @@ public class BoogieSource {
 			}
 			sourcePoint.col += indent;
 		}
-		if (linePointTerm != null) { 
+		if (linePointTerm != null) {
 			// map point for given AST node to current boogie position
 			addPoint(linePointTerm);
 		}
-		
+
 		implBody.append(o);
 		newLine = false;
 		sourcePoint.col += o.toString().length();
 	}
 
 	/**
-	 * Appends an object similar to {@link #append(Object)} but adds a newline after
-	 * the contents have been appended (increasing the line point and resetting indentation).
+	 * Appends an object similar to {@link #append(Object)} but adds a newline
+	 * after the contents have been appended (increasing the line point and
+	 * resetting indentation).
 	 * 
-	 * @param o the object to append (by calling {@link #toString()} on the object).
+	 * @param o
+	 *            the object to append (by calling {@link #toString()} on the
+	 *            object).
 	 */
 	public void appendLine(Object o) {
 		append(o, null);
@@ -163,19 +176,20 @@ public class BoogieSource {
 		sourcePoint.row++;
 		sourcePoint.col = 1;
 	}
-	
+
 	/**
 	 * Increases the indent by one.
 	 */
 	public void increaseIndent() {
 		indent++;
 	}
-	
+
 	/**
 	 * Decreases the indent by one.
 	 */
 	public void decreaseIndent() {
 		indent--;
-		if (indent < 0) indent = 0;
+		if (indent < 0)
+			indent = 0;
 	}
 }

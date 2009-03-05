f631574be0a7210a85c878f6d392aa5fb916dfc8
Jason Forand <j_fora@encs.concordia.ca>
2009-03-05 15:23:37 +0000

    Add tests

--- org.jmlspecs.eclipse.jdt.core.tests/src/org/jmlspecs/eclipse/jdt/core/tests/boogie/BoogieSourceTests.java
+++ org.jmlspecs.eclipse.jdt.core.tests/src/org/jmlspecs/eclipse/jdt/core/tests/boogie/BoogieSourceTests.java
@@ -10,16 +10,64 @@ public class BoogieSourceTests extends TestCase{
 	
 	public void testPrepend() {
 		BoogieSource b = new BoogieSource();
+		
 		assertTrue(b.getResults().equals(BoogieSource.getHeaders()));
+		
 		String prepend = "This will now be prepended";
-		b.preprend(prepend);
+		b.prepend(prepend);
+		
 		assertTrue(b.getResults().equals(BoogieSource.getHeaders() + prepend + "\n"));
 		
 		String toBeAppended = "true";
 		b.append(toBeAppended, new TrueLiteral(0, 4));
+		
 		assertTrue(toBeAppended.equals(b.getTermAtPoint(new BoogieSourcePoint(4, 1)).toString()));
 	}
 	
+	public void testEmpty() {
+		BoogieSource b = new BoogieSource();
+		String prepend = "";
+		b.prepend(prepend);
+		
+		assertTrue(b.getResults().equals(BoogieSource.getHeaders()));
+	}
+	
+	public void testTwoPrepends() {
+		BoogieSource b = new BoogieSource();
+		String prepend1 = "1";
+		String prepend2 = "2";
+		b.prepend(prepend1);
+		
+		assertTrue(b.getResults().equals(BoogieSource.getHeaders()+prepend1+"\n"));
+		
+		b.prepend(prepend2);
+		
+		assertTrue(b.getResults().equals(BoogieSource.getHeaders()+prepend2+"\n"+prepend1+"\n"));
+	}
+	
+	public void testPrependNewline() {
+		BoogieSource b = new BoogieSource();
+		String prepend = "\n";
+		
+		b.prepend(prepend);
+		
+		assertTrue(b.getResults().equals(BoogieSource.getHeaders()+prepend));
+	}
+	
+	public void testManyLineStatement() {
+		BoogieSource b = new BoogieSource();
+		String prepend = "This is a string that needs to be prepended\n" +
+				"However it happens on multiple lines";
+		
+		b.prepend(prepend);
+		
+		assertTrue(b.getResults().equals(BoogieSource.getHeaders()+prepend+"\n"));
+		
+		prepend += "\n";
+		b.prepend(prepend);
+		
+		assertTrue(b.getResults().equals(BoogieSource.getHeaders()+prepend+prepend));
+	}
 	
 
 }

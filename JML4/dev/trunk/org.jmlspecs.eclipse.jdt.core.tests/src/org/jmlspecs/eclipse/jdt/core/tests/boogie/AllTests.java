51d115a450ac41de86a0e9e321c0da430cf8dc8e
Loren Segal <lsegal@soen.ca>
2009-03-05 22:05:39 +0000

    Add suite to run all tests. We should use this.

---
+++ JML4/dev/trunk/org.jmlspecs.eclipse.jdt.core.tests/src/org/jmlspecs/eclipse/jdt/core/tests/boogie/AllTests.java
@@ -0,0 +1,20 @@
+package org.jmlspecs.eclipse.jdt.core.tests.boogie;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for org.jmlspecs.eclipse.jdt.core.tests.boogie");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(InitialTests.class);
+		suite.addTestSuite(BoogieSourceTests.class);
+		suite.addTestSuite(BoogieSymbolTableTests.class);
+		suite.addTestSuite(AdapterTests.class); // adapters go last
+		//$JUnit-END$
+		return suite;
+	}
+
+}

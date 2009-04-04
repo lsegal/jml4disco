b21966f8098fb7e2789102640e1da844a6a2bb43
Loren Segal <lsegal@soen.ca>
2009-04-04 19:03:03 +0000

    Add ability to turn off surrounding parentheses in boogie.ast.BinaryExpression

--- org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/ast/BinaryExpression.java
+++ org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/ast/BinaryExpression.java
@@ -6,24 +6,34 @@ import org.jmlspecs.jml4.boogie.BoogieSource;
 public class BinaryExpression extends Expression {
 	private Expression left, right;
 	private String operator;
+	private boolean surroundWithParen;
 	
 	public BinaryExpression(Expression left, String operator, Expression right, ASTNode javaNode, Scope scope) {
+		this(left, operator, right, true, javaNode, scope);
+	}
+	
+	public BinaryExpression(Expression left, String operator, Expression right, boolean surroundWithParen, ASTNode javaNode, Scope scope) {
 		super(javaNode, scope);
+		this.surroundWithParen = surroundWithParen;
 		this.left = left;
 		this.right = right;
 		this.operator = operator;
 	}
 
+	public boolean isSurroundedWithParen() { 
+		return surroundWithParen;
+	}
+	
 	public Expression getLeft() { return left; }
 	public Expression getRight() { return right; }
 	public String getOperator() { return operator; }
 
 	public void toBuffer(BoogieSource out) {
-		out.append(TOKEN_LPAREN);
+		if (isSurroundedWithParen()) out.append(TOKEN_LPAREN);
 		getLeft().toBuffer(out);
 		out.append(TOKEN_SPACE + getOperator() + TOKEN_SPACE); 
 		getRight().toBuffer(out);
-		out.append(TOKEN_RPAREN);
+		if (isSurroundedWithParen()) out.append(TOKEN_RPAREN);
 	}
 
 	public void traverse(Visitor visitor) {

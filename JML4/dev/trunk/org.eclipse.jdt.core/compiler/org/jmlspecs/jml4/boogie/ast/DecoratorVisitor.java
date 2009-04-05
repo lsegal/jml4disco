a8c86785f247a66973d8664cc10117756efb73fd
Loren Segal <lsegal@soen.ca>
2009-04-05 04:32:44 +0000

    Handle modifies clauses and fix types during resolution pass

--- JML4/dev/trunk/org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/ast/DecoratorVisitor.java
+++ JML4/dev/trunk/org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/ast/DecoratorVisitor.java
@@ -8,6 +8,9 @@ public class DecoratorVisitor extends Visitor {
 			resolveTypes((TypeDeclaration)term.getTypes().get(i));
 		}
 
+		term.getStatements().add(new Comment("/*!BOOGIESTART!*/")); //$NON-NLS-1$
+		
+		resolveGlobals(term.getGlobals());
 		for (int i = 0; i < term.getProcedures().size(); i++) {
 			((BoogieNode)term.getProcedures().get(i)).traverse(this);
 		}
@@ -20,13 +23,15 @@ public class DecoratorVisitor extends Visitor {
 			((BoogieNode)term.getStatements().get(i)).traverse(this);
 		}
 
-		resolveProcedureLocals(term.getLocals());
-		
-		// arguments must be added after locals traversal
-		for (int i = 0; i < term.getArguments().size(); i++) {
+		for (int i = 0, count = 0; i < term.getArguments().size(); i++) {
 			VariableDeclaration var = (VariableDeclaration)term.getArguments().get(i);
-			resolveProcedureArguments(var);
+			BinaryExpression expr = typeCheckExpression(var);
+			if (expr != null) {
+				var.getScope().getProcedureScope().getRequires().add(count++, expr);
+			}
 		}
+
+		resolveLocals(term.getLocals());
 		
 		return false;
 	}
@@ -35,6 +40,13 @@ public class DecoratorVisitor extends Visitor {
 		VariableDeclaration decl = term.getScope().lookupVariable(term.getVariable().getName());
 		if (decl != null && !decl.isLocal()) {
 			Procedure proc = term.getScope().getProcedureScope();
+			// don't add duplicates
+			for (int i = 0; i < proc.getModifies().size(); i++) {
+				if (proc.getModifies().get(i) instanceof VariableReference) {
+					VariableReference ref = (VariableReference)proc.getModifies().get(i);
+					if (ref.getName().equals(decl.getName().getName())) return false;
+				}
+			}
 			proc.getModifies().add(decl.getName());
 		}
 		return false;
@@ -44,9 +56,13 @@ public class DecoratorVisitor extends Visitor {
 		if (term.getOutVar() == null) {
 			Procedure calledProcedure = term.getScope().lookupProcedure(term.getProcedureName());
 			if (calledProcedure != null) { // calledProcedure should not be null
+				Procedure proc = term.getScope().getProcedureScope();
+				
+				// check if procedure is called without outvar when spec defines one
+				// Boogie needs any method call to assign to as many out vars as in the procedure
+				// declaration
 				if (calledProcedure.getReturnType() != null) {
 					// need to declare a variable
-					Procedure proc = term.getScope().getProcedureScope();
 					String varName = "$unused_" + proc.getLocals().size(); //$NON-NLS-1$
 					VariableDeclaration decl = new VariableDeclaration(
 							new VariableReference(varName, null, proc),  
@@ -54,6 +70,11 @@ public class DecoratorVisitor extends Visitor {
 					proc.addVariable(decl);
 					term.setOutVar(decl.getName());
 				}
+				
+				// add all modifies, requires and ensures clauses to callee
+				proc.getModifies().addAll(calledProcedure.getModifies());
+				//proc.getEnsures().addAll(calledProcedure.getEnsures());
+				//proc.getRequires().addAll(calledProcedure.getRequires());
 			}
 		}
 		return false;
@@ -67,26 +88,49 @@ public class DecoratorVisitor extends Visitor {
 				new TokenLiteral(term.getSuperType().getTypeName()), 
 				false, null, term.getScope()), null, term.getScope()));
 	}
-	
-	private void resolveProcedureArguments(VariableDeclaration var) {
-		var.getScope().addVariable(var);
-		
-		// type requirements
-		// TODO add support for array types
-		if (!(var.getType() instanceof MapTypeReference) && !var.getType().isNative()) {
-			Procedure proc = var.getScope().getProcedureScope();
-			BinaryExpression expr = new BinaryExpression(
-					new FunctionCall("$dtype", new Expression[] { var.getName() }, var.getJavaNode(), proc),  //$NON-NLS-1$
-					"==", new TokenLiteral(var.getType().getTypeName()), false, null, proc); //$NON-NLS-1$
-			proc.getRequires().add(expr);
-		}
-	}
 
-	private void resolveProcedureLocals(ArrayList vars) {
+	private void resolveLocals(ArrayList vars) {
 		for (int i = 0; i < vars.size(); i++) {
 			VariableDeclaration var = (VariableDeclaration)vars.get(i);
 			Procedure proc = var.getScope().getProcedureScope();
-			proc.getStatements().add(0, new VariableStatement(var, var.getJavaNode(), proc));
+			proc.getStatements().add(i, new VariableStatement(var, var.getJavaNode(), proc));
 		}
+		for (int i = 0, count = 0; i < vars.size(); i++) {
+			VariableDeclaration var = (VariableDeclaration)vars.get(i);
+			BinaryExpression expr = typeCheckExpression(var);
+			if (expr != null) {
+				Procedure proc = var.getScope().getProcedureScope();
+				proc.getStatements().add(vars.size() + count++, new AssumeStatement(expr, var.getJavaNode(), proc));
+			}
+		}
+	}
+	
+	private void resolveGlobals(ArrayList vars) {
+		for (int i = 0; i < vars.size(); i++) {
+			VariableDeclaration var = (VariableDeclaration)vars.get(i);
+			Program prog = var.getScope().getProgramScope();
+			prog.getStatements().add(new VariableStatement(var, var.getJavaNode(), prog));
+		}
+		for (int i = 0; i < vars.size(); i++) {
+			VariableDeclaration var = (VariableDeclaration)vars.get(i);
+			BinaryExpression expr = typeCheckExpression(var);
+			if (expr != null) {
+				Program prog = var.getScope().getProgramScope();
+				prog.getStatements().add(new Axiom(expr, var.getJavaNode(), prog));
+			}
+		}
+	}
+
+	private BinaryExpression typeCheckExpression(VariableDeclaration var) {
+		// type requirements
+		// TODO add support for array types
+		if (!(var.getType() instanceof MapTypeReference) && !var.getType().isNative()) {
+			return new BinaryExpression(new FunctionCall("$dtype", //$NON-NLS-1$ 
+					new Expression[] { var.getName() }, var.getJavaNode(), var.getScope()), 
+					"==", new TokenLiteral(var.getType().getTypeName()), false, //$NON-NLS-1$
+					var.getName().getJavaNode(), var.getScope()); 
+		}
+		return null;
+		
 	}
 }

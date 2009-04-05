3eec92b55a1f33950983e931953330c05b6a12f5
Loren Segal <lsegal@soen.ca>
2009-04-05 04:29:35 +0000

    Add invariants to WhileStatement node

--- org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/ast/WhileStatement.java
+++ org.eclipse.jdt.core/compiler/org/jmlspecs/jml4/boogie/ast/WhileStatement.java
@@ -7,16 +7,22 @@ import org.jmlspecs.jml4.boogie.BoogieSource;
 
 public class WhileStatement extends Statement {
 	private ArrayList/*<Statement>*/ statements;
+	private ArrayList/*<Expression>*/ invariants;
 	private Expression condition;
 	
 	public WhileStatement(Expression condition, ASTNode javaNode, Scope scope) {
 		super(javaNode, scope);
 		this.condition = condition;
 		this.statements = new ArrayList();
+		this.invariants = new ArrayList();
 	}
 	
 	public ArrayList getStatements() {
 		return statements;
+	}
+	
+	public ArrayList getInvariants() {
+		return invariants;
 	}
 	
 	public Expression getCondition() {
@@ -26,14 +32,23 @@ public class WhileStatement extends Statement {
 	public void toBuffer(BoogieSource out) {
 		out.append("while" + TOKEN_SPACE); //$NON-NLS-1$
 		if (getCondition() instanceof BinaryExpression) {
+			getCondition().toBuffer(out);
+		}
+		else {
 			out.append(TOKEN_LPAREN);
 			getCondition().toBuffer(out);
 			out.append(TOKEN_RPAREN);
 		}
-		else {
-			getCondition().toBuffer(out);
+		if (getInvariants().size() > 0) {
+			out.append(TOKEN_SPACE);
 		}
-
+		for (int i = 0; i < getInvariants().size(); i++) {
+			Expression invariant = (Expression)getInvariants().get(i);
+			out.append("invariant" + TOKEN_SPACE, invariant.getJavaNode()); //$NON-NLS-1$
+			invariant.toBuffer(out);
+			out.append(TOKEN_SEMICOLON);
+		}
+			
 		out.appendLine(TOKEN_SPACE + TOKEN_LBRACE);
 		out.increaseIndent();
 		for (int i = 0; i < getStatements().size(); i++) {
